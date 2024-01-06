3cec0cdaff246b2b5b972c60c362405016fdfc4c
christinen22 <christine.elin.louise@gmail.com>
2024-01-06 12:04:18 +0100

    error deploy fix

--- recipe-rhapsody/app/components/search/SearchIngredientsComp.tsx
+++ recipe-rhapsody/app/components/search/SearchIngredientsComp.tsx
@@ -5,8 +5,8 @@ const SearchIngredientsComp = () => {
   return (
     <div className={styles.container}>
       <span className={styles.text}>
-        Do you have a set of ingredients at home and don't know what to do with
-        it?
+        Do you have a set of ingredients at home and don&rsquo;t know what to do
+        with it?
         <br />
         <br />
         Perform a search and see what suggestions you get!
